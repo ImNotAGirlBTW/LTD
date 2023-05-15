7ea50bfefe71b70b15684a3eddd6ab674999eb11
unknown <theo.krok@seznam.cz>
2023-05-15 22:12:36 +0200

    f

--- Assets/StaticSpawnerScript.cs
+++ Assets/StaticSpawnerScript.cs
@@ -14,7 +14,7 @@ public class StaticSpawnerScript : MonoBehaviour
     {
                for(int i =0; i < Random.Range(10,20);i++)
         {
-            if(Random.Range(0f,1f) <= spChance){
+            if(Random.Range(0f,1f) <=  spChance){
          Instantiate(fEn, transform.position +  new Vector3(Random.Range(xRange,yRange) + 2, Random.Range(-8f,2f) +2,0.0f), Quaternion.identity);
             }else{
     Instantiate(bEn, transform.position +  new Vector3(Random.Range(xRange,yRange) + 2, Random.Range(-8f,2f) +2,0.0f), Quaternion.identity);
